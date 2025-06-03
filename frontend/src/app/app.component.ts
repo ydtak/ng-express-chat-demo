6f5d8dbbedd2ee58d974157a6783a1492bc2ecd7
Daniel Takayama <ydtak@umich.edu>
2025-06-03 01:49:32 -0400

    Setup tailwind and add empty chat component.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
